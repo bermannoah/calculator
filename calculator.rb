1a16f020afce784522915dc89757d299e110ccc3
Noah Berman <noah@noahberman.org>
2016-07-05 13:18:20 -0500

    Made the words slightly better

--- calculator.rb
+++ calculator.rb
@@ -1,5 +1,5 @@
 puts "Enter done when you've added all the numbers you need."
-puts "This might not work so well with 0. Just a warning."
+puts "This might not work so well with 0. Just so you know."
 
 # Creates an array called numbers. Iterates various operations through the array
 # based on user input.
@@ -10,8 +10,9 @@ user_input = nil
 while user_input != 'done'                        # Gets user input unless they type "done"
 print "What number do you want to include? > "
   user_input = gets.chomp
-  numbers.push(user_input.to_f)
+  numbers.push(user_input.to_f)                   # for division's sake we'll convert to floats. I'm sure there's a cleaner way to do this...
 end
+
   print "Would you like to add (1), subtract (2), multiply (3), or divide (4) these numbers? > "    # Gives user options for calculations.
   operation = gets.to_i
 
@@ -24,5 +25,5 @@ end
   elsif operation == 4
     puts "The quotient is #{numbers.reject(&:zero?).inject(:/)}"
   else
-    puts "That is not an option, sorry."
+    puts "That is not an option, sorry. Please try again."
 end

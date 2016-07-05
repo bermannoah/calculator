75cba6f5b9e09ada29d507176dd97d71332eb0d0
Noah Berman <noah@noahberman.org>
2016-07-05 13:12:04 -0500

    fixes operations, makes math work, turns everything into float

--- calculator.rb
+++ calculator.rb
@@ -4,19 +4,19 @@ user_input = nil
 while user_input != 'done'
 print "What number do  you want to include? > "
   user_input = gets.chomp
-  numbers.push(user_input.to_i)
+  numbers.push(user_input.to_f)
 end
   print "Would you like to add (1), subtract (2), multiply (3), or divide (4) these numbers? > "
   operation = gets.to_i
 
   if operation == 1
-    puts "The sum is #{numbers.reduce(0, :+)}"
+    puts "The sum is #{numbers.reject(&:zero?).reduce(:+)}"
   elsif operation == 2
-    puts "The remainder is #{numbers.reduce(0, :-)}"
+    puts "The remainder is #{numbers.reject(&:zero?).reduce(:-)}"
   elsif operation == 3
-    puts "The product is #{numbers.reduce(1, :*)}"
+    puts "The product is #{numbers.reject(&:zero?).inject(:*)}"
   elsif operation == 4
-    puts "The quotient is #{numbers.reduce(1, :/)}"
+    puts "The quotient is #{numbers.reject(&:zero?).inject(:/)}"
   else
     puts "That is not an option, sorry."
 end

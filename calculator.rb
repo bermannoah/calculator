82ee1c2bc832cf7df14c58395f051b38c6a2c839
Noah Berman <noah@noahberman.org>
2016-07-05 13:16:25 -0500

    adds comments, little bit of cleanup

--- calculator.rb
+++ calculator.rb
@@ -1,18 +1,21 @@
 puts "Enter done when you've added all the numbers you need."
 puts "This might not work so well with 0. Just a warning."
 
+# Creates an array called numbers. Iterates various operations through the array
+# based on user input.
+
 numbers = []
 
 user_input = nil
-while user_input != 'done'
+while user_input != 'done'                        # Gets user input unless they type "done"
 print "What number do you want to include? > "
   user_input = gets.chomp
   numbers.push(user_input.to_f)
 end
-  print "Would you like to add (1), subtract (2), multiply (3), or divide (4) these numbers? > "
+  print "Would you like to add (1), subtract (2), multiply (3), or divide (4) these numbers? > "    # Gives user options for calculations.
   operation = gets.to_i
 
-  if operation == 1
+  if operation == 1                                               # Depending on user selection, does the math or quits.
     puts "The sum is #{numbers.reject(&:zero?).reduce(:+)}"
   elsif operation == 2
     puts "The remainder is #{numbers.reject(&:zero?).reduce(:-)}"
